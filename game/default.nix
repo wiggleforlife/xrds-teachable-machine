5d88914e8f37613319525180d0846944b462a8b4
Cian Ormond <co@cianormond.com>
2024-02-12 13:39:21 -0800

    WASM exports, move to bevy_mod_reqwest for async (wasm doesn't support blocking)

--- game/default.nix
+++ game/default.nix
@@ -9,14 +9,17 @@ mkShell rec {
     pkg-config
   ];
   buildInputs = [
-    fenix.latest.toolchain #TODO add targets.wasm32-unknown-unknown.stable.rust-std
+    (with fenix; combine [
+        stable.cargo stable.clippy stable.rust-src stable.rustc stable.rustfmt targets.wasm32-unknown-unknown.stable.rust-std
+     ])
     udev alsa-lib vulkan-loader
-    clang lld mold # TODO lld/mold sometimes cannot find dylibs
+    clang mold
     xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr
     libxkbcommon wayland
-    openssl
+    openssl wasm-bindgen-cli
   ];
   LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-
-  # shellHook = "rust-rover .";
 }
+
+# cargo build --release --target wasm32-unknown-unknown && wasm-bindgen --no-typescript --target web --out-dir ./out/ --out-name "xrds-teachable-machine" ./target/wasm32-unknown-unknown/release/xrds-teachable-machine.wasm
+
--- game/src/main.rs
+++ game/src/main.rs
@@ -1,9 +1,15 @@
+use bevy::asset::AssetMetaCheck;
 use std::str::FromStr;
+use std::time::Duration;
 
 use bevy::prelude::*;
+use bevy::time::common_conditions::on_timer;
 use bevy::window::WindowResolution;
+use bevy_mod_reqwest::bevy_eventlistener::callbacks::ListenerInput;
+use bevy_mod_reqwest::reqwest::header::ACCESS_CONTROL_ALLOW_ORIGIN;
+use bevy_mod_reqwest::reqwest::{Request, Url};
+use bevy_mod_reqwest::*;
 use rand::Rng;
-use reqwest::blocking::Client;
 
 const RES: UVec2 = UVec2::new(1280, 720);
 
@@ -15,22 +21,32 @@ const OBSTACLE_SPAWN: [Vec3; 2] =
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: String::from("Bing bong"),
-                resolution: WindowResolution::new(1280., 720.)
-                    .with_scale_factor_override(1.),
-                resizable: false,
+        .insert_resource(AssetMetaCheck::Never)
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: String::from("Bing bong"),
+                    resolution: WindowResolution::new(1280., 720.)
+                        .with_scale_factor_override(1.),
+                    resizable: false,
 
+                    ..default()
+                }),
                 ..default()
             }),
-            ..default()
-        }),))
+            ReqwestPlugin::default(),
+        ))
         .add_systems(Startup, setup)
         .add_systems(
             Update,
-            (spawn_obstacles, update_car_position, update_obstacle_position),
+            (
+                spawn_obstacles,
+                update_car_position,
+                update_obstacle_position,
+                get_body_position.run_if(on_timer(Duration::from_millis(100))),
+            ),
         )
+        .add_event::<PositionGetEvent>()
         .run();
 }
 
@@ -43,9 +59,6 @@ struct Car;
 #[derive(Component)]
 struct Obstacle;
 
-#[derive(Resource)]
-struct HttpClient(Client);
-
 #[derive(Clone, Default, Resource)]
 struct Assets {
     background: Handle<Image>,
@@ -53,7 +66,9 @@ struct Assets {
     obstacles: Vec<Handle<Image>>,
 }
 
-fn setup(mut commands: Commands, server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands, mut client: BevyReqwest, server: Res<AssetServer>,
+) {
     let assets = Assets {
         background: server.load("background.png"),
         car: server.load("car.png"),
@@ -63,7 +78,16 @@ fn setup(mut commands: Commands, server: Res<AssetServer>) {
         ],
     };
     commands.insert_resource(assets.clone());
-    commands.insert_resource(HttpClient(Client::new()));
+
+    let url: Url = "http://localhost:5000/api/pos".try_into().unwrap();
+    let req = client
+        .client()
+        .get(url)
+        .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        .build()
+        .expect("Failed to make GET request");
+
+    commands.insert_resource(RequestResource(req));
 
     commands.spawn((Camera2dBundle::default(), MainCamera));
     commands.spawn(SpriteBundle { texture: assets.background, ..default() });
@@ -109,21 +133,38 @@ fn spawn_obstacles(
     }
 }
 
+#[derive(Event)]
+struct PositionGetEvent(String);
+impl From<ListenerInput<ReqResponse>> for PositionGetEvent {
+    fn from(value: ListenerInput<ReqResponse>) -> Self {
+        let s = value.as_string().unwrap();
+        PositionGetEvent(s)
+    }
+}
+
+#[derive(Resource)]
+struct RequestResource(Request);
+
+fn get_body_position(mut client: BevyReqwest, req: Res<RequestResource>) {
+    client.send(
+        req.0.try_clone().expect("Failed to clone request"),
+        On::send_event::<PositionGetEvent>(),
+    );
+}
+
 fn update_car_position(
+    mut events: EventReader<PositionGetEvent>,
     mut car: Query<(&mut Transform, &mut Sprite), With<Car>>,
-    client: Res<HttpClient>,
 ) {
+    let event = events.read().last();
+    if event.is_none() {
+        return;
+    }
+    let event = event.unwrap();
     let mut car = car.single_mut();
 
-    //TODO slow down
-    let res = client
-        .0
-        .get("http://127.0.0.1:5000/api/pos")
-        .send()
-        .expect("Failed to make GET request");
-    let body = &*res.text().expect("Failed to read response body");
-    let x = i32::from_str(body)
-        .expect(&format!("Failed to parse response body - {:?}", body));
+    let x = i32::from_str(&*event.0)
+        .expect(&format!("Failed to parse response body - {:?}", &*event.0));
 
     (car.0.translation, car.1.flip_x) =
         if x > 0 { (CAR_RIGHT_LANE, true) } else { (CAR_LEFT_LANE, false) };
